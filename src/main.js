2e455b355f341d28fb0c7d2995b7df1365c7e7e3
Boris <boris260692@gmail.com>
2022-11-04 20:11:30 +0200

    Added new block

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ let ts_tit = document.querySelector('.ts_tit')
 let ts_sec = document.querySelector('.ts_sec')
 let box_ts = document.querySelector('.block_with_comp_ts')
 let confirm_ts = document.querySelector('.confirm_ts')
-
+let save = document.querySelector('.save')
 
 confirm_ts.onclick =()=>{
     if (ts_tit.value == false || ts_sec.value == false) {
@@ -18,19 +18,20 @@ confirm_ts.onclick =()=>{
                 <p class='comp_ts_tit'>${ts_tit.value}</p>
                 <p class='comp_ts_sec'>${ts_sec.value}</p>
                 <div class='ts_btn_wrapp'>
-                    <button class='hide_comp_ts'>Виконано</button>
+                    <button class='hide_comp_ts'>Виконано / Повернути</button>
                     <button class='del_comp_ts'>X</button>
                 </div>
             </div>
         </div>
-    `
+        `
     }
     ts_tit.value = ''
     ts_sec.value = ''
 }
+
 box_ts.addEventListener('click', function(e) {
-  if (e.target.closest('.hide_comp_ts'))
-  e.target.closest('.ts_content').classList.add('ts_content_remov');
+    if (e.target.closest('.hide_comp_ts'))
+  e.target.closest('.ts_content').classList.toggle('ts_content_remov');
   if (e.target.closest('.del_comp_ts'))
   e.target.closest('.ts_content').remove();
 });
