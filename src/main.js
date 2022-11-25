e83c2c53fec13ef41ed806d47c8a4737dc4d52af
Boris <boris260692@gmail.com>
2022-11-25 13:27:48 +0200

    Added task manager with new JS

--- src/main.js
+++ src/main.js
@@ -2,90 +2,81 @@ import './styles/main.scss';
 import './bootstrap.js';
 import $ from "jquery"; // бібліотека jquery
 
-// Форми
-let regForm = document.querySelector('.register_form')
-let autForm = document.querySelector('.autorization_form')
+let ts_tit = document.querySelector('.ts_tit')
+let ts_sec = document.querySelector('.ts_sec')
+let box_ts = document.querySelector('.block_with_comp_ts')
+let confirm_ts = document.querySelector('.confirm_ts')
 
-// Поля реєстрації
-let loginReg = document.querySelector('.reg_login')
-let ageReg = document.querySelector('.reg_age')
-let passReg = document.querySelector('.reg_pass')
-let passRegConfirm = document.querySelector('.reg_confirm_pass')
-
-//Поля авторизації
-let loginConfirm = document.querySelector('.autentification_login')
-let passConfirm = document.querySelector('.autentification_pass')
-
-// Кнопки
-const register = document.querySelector('.register_btn')
-const autorization = document.querySelector('.autorization_btn')
-const loginBtn = document.querySelector('.enter_btn')
-const registerLink = document.querySelector('.go_register')
-
-// Текст відсутності користувача
-const alert_txt = document.querySelector('.alert_txt')
-// Авторизований користувач
-const confirm_txt = document.querySelector('.confirmed_person')
-
-let persons = []
-const tasksTemp = localStorage.getItem('person');
+let tasks = [];
+const tasksTemp = localStorage.getItem('tasks');
 if (tasksTemp && tasksTemp !== null) {
     try {
-        persons = JSON.parse(tasksTemp);
+        tasks = JSON.parse(tasksTemp);
     } catch (error) {
         console.log(error);
     }
 }
 
-registerLink.onclick = () =>{
-    autForm.style.display = 'none'
-    regForm.style.display = 'flex'
-    alert_txt.innerHTML = ''
-    alert_txt.style.display = `none`
-    confirm_txt.style.display = `none`
-}
-autorization.onclick = () =>{
-    autForm.style.display = 'flex'
-    regForm.style.display = 'none'
-}
-loginBtn.onclick = () =>{
-    let raw = localStorage.getItem('person')
-    let parsePersons = JSON.parse(raw)
-    for (let i = 0; i < parsePersons.length; i++) {
-        let registerPerson = parsePersons.find(person => person.login == loginConfirm.value.toUpperCase() && person.password == passConfirm.value.toUpperCase())
-        if (registerPerson != undefined){
-            confirm_txt.innerHTML = `Вітаю ${registerPerson.login}, авторизація пройшла успішно`
-            confirm_txt.style.display = `block`
-            alert_txt.style.display = `none`
-        } else {
-            alert_txt.innerHTML = 'Користувача не знайдено'
-            confirm_txt.style.display = `none`
-            alert_txt.style.display = `block`
-        }
+const remove = (id) => {
+    document.getElementById(id).remove();
+    tasks = tasks.filter(item => item?.id !== id);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+const doneUndone = (id) => {
+    let done = document.getElementById(id).classList.contains('ts_content_remov');
+    if (done) {
+        document.getElementById(id).classList.remove('ts_content_remov');
+    } else {
+        document.getElementById(id).classList.add('ts_content_remov');
     }
+    tasks = tasks.map(item => {
+        if (item.id === id) {
+            item.done = !done;
+        }
+        return item;
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+
+window.remove = remove;
+window.doneUndone = doneUndone;
+
+const addHtml = ({ id = new Date().getTime(), title, desc, done = false }) => {
+    box_ts.innerHTML += `
+        <div class='ts_content${done ? ' ts_content_remov' : ''}' id='${id}'>
+            <div class='coomp_ts_wrapp'>
+                <p class='comp_ts_tit'>${title}</p>
+                <p class='comp_ts_sec'>${desc}</p>
+                <div class='ts_btn_wrapp'>
+                    <button onclick='doneUndone(${id})' class='hide_comp_ts'>Виконано / Повернути</button>
+                    <button onclick='remove(${id})' class='del_comp_ts'>X</button>
+                </div>
+            </div>
+        </div>
+        `
 }
 
-register.onclick = () => {
-    if (passReg.value == passRegConfirm.value) {
-        if (loginReg.value == false && ageReg.value == false && passReg.value == false &&  passRegConfirm.value == false){
-            alert('Необхідно заповнити всі поля')
-        } else {
-            let obj = {
-                login: loginReg.value.toUpperCase(),
-                age: ageReg.value,
-                password: passReg.value.toUpperCase()
-            }
-            persons.push(obj)
-            localStorage.setItem('person', JSON.stringify(persons))
-            loginReg.value = ''
-            ageReg.value = ''
-            passReg.value = ''
-            passRegConfirm.value = ''
-            alert('Вітаємо, реєстрація успішна, перейдіть до входу')
-            autForm.style.display = 'flex'
-            regForm.style.display = 'none'
-        }
+// при старті апп
+for (let i = 0; i < tasks.length; i++) {
+    addHtml(tasks[i]);
+}
+
+confirm_ts.onclick = () => {
+    if (ts_tit.value == false || ts_sec.value == false) {
+        alert('Заповніть будь-ласка назву та опис завдання')
     } else {
-        alert('Паролі не співпадають')
+        const obj = {
+            id: new Date().getTime(),
+            title: ts_tit.value,
+            desc: ts_sec.value,
+            done: false
+        };
+        addHtml(obj);
+        tasks.push(obj);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+        ts_tit.value = ''
+        ts_sec.value = ''
     }
 }
